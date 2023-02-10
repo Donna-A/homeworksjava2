6b6cd48b5286e5f2720c34749dce9cb8a2eeb7f8
Donna <annamirnow@gmail.com>
2023-02-10 15:11:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 15678; // Стоимость билета, руб
+        int onemile = 20; // Количество рублей для одной бонусной мили
+
+
+        System.out.println("Количество бонусных миль:");
+        System.out.println(price / onemile);
+    }
+}
